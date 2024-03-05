ddfac9b9fb0e506a25bd234f6e2f679d38d67a3b
Andrew Brehm <andrew.brehm@dialexa.com>
2024-03-05 18:31:48 -0500

    move dirs

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(
             name: "CommonUI",
             targets: ["CommonUI", "FeatureView"]),
-    ],
+    ], 
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
